bd59f816ead8b4f8ce3416180c7fc04831a5b063
shdakts <suhedaakts@gmail.com>
2024-05-09 01:18:57 +0300

    Add project files.

---
+++ piramit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace demo
+{
+    class piramit
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Piramitin basamak değerini giriniz:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                for (int k = i; k <= 2 * i - 1; k++)
+                {
+                    Console.Write(k);
+                }
+                for (int l = 2 * i - 2; l >= i; l--)
+                {
+                    Console.Write(l);
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
